fb34f3f47fb092f90d89f8f384723fc974fb0b64
iurascualexandru <alexandruiurascu@yahoo.com>
2022-09-20 10:12:15 +0300

    hero-section

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,10 @@ const addTwo = v => v + 2;
 const y = addTwo(x);
 // console.log(y);
 
+const modal = document.getElementsByClassName(`modal`);
+const menuBtn = document.getElementsByClassName(`modal-menu`);
+const closeBtn = document.getElementsByClassName(`close-icon`);
+
+// document.addEventListener("click", function () {
+//     modal.classList.remove("display-none");
+// });
