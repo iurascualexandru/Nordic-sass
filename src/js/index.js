d2dc0809fceb5a5dda48e43a3477f79e2ea3d365
iurascualexandru <alexandruiurascu@yahoo.com>
2022-09-21 09:14:48 +0300

    incercari modal + bug fix-uri

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,14 @@ const addTwo = v => v + 2;
 const y = addTwo(x);
 // console.log(y);
 
-const modal = document.getElementsByClassName(`modal`);
-const menuBtn = document.getElementsByClassName(`modal-menu`);
-const closeBtn = document.getElementsByClassName(`close-icon`);
+// console.log(`I'm gonna test this`);
 
-// document.addEventListener("click", function () {
-//     modal.classList.remove("display-none");
+// const modal = document.getElementsByClassName(`modal`);
+// const menuBtn = document.getElementsByClassName(`modal-menu`);
+// const closeBtn = document.getElementsByClassName(`close-icon`);
+
+// menuBtn.addEventListener("click", function () {
+//     modal.style.display = "block";
+//     console.log(`test`);
+//     // modal.classList.remove("display-none");
 // });
