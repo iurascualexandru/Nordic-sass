e7de8e89b95eb34269294eb51a4fc60971aaa938
iurascualexandru <alexandruiurascu@yahoo.com>
2022-09-22 13:54:33 +0300

    Modals ✔️

--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,60 @@ const y = addTwo(x);
 
 // console.log(`I'm gonna test this`);
 
-// const modal = document.getElementsByClassName(`modal`);
-// const menuBtn = document.getElementsByClassName(`modal-menu`);
-// const closeBtn = document.getElementsByClassName(`close-icon`);
 
-// menuBtn.addEventListener("click", function () {
-//     modal.style.display = "block";
-//     console.log(`test`);
-//     // modal.classList.remove("display-none");
-// });
+
+// Menu Modal
+const modalOpenTriggers = document.querySelectorAll(`[data-role="modal"]`);
+modalOpenTriggers.forEach(el => el.addEventListener(`click`, e => {
+    const el = e.currentTarget;
+    const dataSet = el.dataset;
+    let { target, status } = e.currentTarget.dataset;
+    const modalTarget = document.querySelector(`#${target}`);
+    if (status === `closed`) {
+        modalTarget.style.display = "block";
+
+    } else {
+        modalTarget.style.display = "none";
+    }
+}))
+
+
+// Cart Modal
+const cartModal = document.querySelector(`#cart-modal`);
+const overlay = document.querySelector(`.overlay`);
+document.addEventListener(`click`, function (event) {
+    if (event.target.closest(`.cart-button`)) {
+        this.body.style.overflow = `hidden`;
+        cartModal.style.display = "block";
+        overlay.style.display = `flex`;
+    }
+    else if (event.target.closest(`.close-modal`)) {
+        this.body.style.overflow = `auto`;
+        cartModal.style.display = "none";
+        overlay.style.display = `none`;
+
+    }
+    else if (cartModal.style.display == "block" && !event.target.closest(`#cart-modal`)) {
+        this.body.style.overflow = `auto`;
+        cartModal.style.display = "none";
+        overlay.style.display = `none`;
+    }
+
+})
+
+// LOGIN Modal
+
+const loginModal = document.querySelector(`.login`);
+document.addEventListener(`click`, function (event) {
+    if (event.target.closest(`.account-button`)) {
+        overlay.style.display = `flex`;
+        this.body.style.overflow = `hidden`;
+        loginModal.style.display = `block`;
+    }
+    else if (event.target.closest(`.close-login`) || (loginModal.style.display == "block" && !event.target.closest(`.login`))) {
+        this.body.style.overflow = `auto`;
+        loginModal.style.display = "none";
+        overlay.style.display = `none`;
+    }
+
+})
